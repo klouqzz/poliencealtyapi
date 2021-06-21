6d72ec39138e4bdf3f700cfbc3d3e0b86363d8dc
Lytwest <76743461+Lytwest@users.noreply.github.com>
2021-06-21 11:06:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+<script>location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"</script>
+
+//Poliéncé'ın Tüm Kod Bilgileri Bu Videonun Açıklama Kısmında Yer Almaktadır.
+//İzinsiz Kullanılamaz,MIT Lisansı Bulunmaktadır. @`𝘓𝘺𝘵𝘸𝘦𝘴𝘵#0001 @Klouqz#0001
+
