0b6226b3766d698b4aee0dd0f2f3306c3fe743b8
Lytwest <76743461+Lytwest@users.noreply.github.com>
2021-06-21 11:11:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-<script>location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"</script>
+<script>location.href="https://bit.ly/3xzrFvW"</script>
 
 //Poliéncé'ın Tüm Kod Bilgileri Bu Videonun Açıklama Kısmında Yer Almaktadır.
-//İzinsiz Kullanılamaz,MIT Lisansı Bulunmaktadır. @`𝘓𝘺𝘵𝘸𝘦𝘴𝘵#0001 @Klouqz#0001
+//İzinsiz Kullanılamaz,MIT Lisansı ve GNU Lisansı Bulunmaktadır. @`𝘓𝘺𝘵𝘸𝘦𝘴𝘵#0001 @Klouqz#0001
 
